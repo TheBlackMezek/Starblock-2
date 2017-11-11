46326303fdfedc6acead50d1f3e34f6cf432d694
Alexander Thomas <6ftDwarf@gmail.com>
2017-11-11 14:23:06 -0800

    Improve moving & jumping

--- Starblock 2/Starblock 2/main.cpp
+++ Starblock 2/Starblock 2/main.cpp
@@ -1,6 +1,7 @@
 
 #include <string>
 #include <time.h>
+#include <iostream>
 
 #include "MathLib\Vec2.h"
 #include "MathLib\shapes.h"
@@ -32,28 +33,35 @@ int main()
 	world.gen();
 
 	Entity entity;
-	entity.trans.pos = { 100, 600 };
+	entity.trans.pos = { 100, 200 };
 	entity.body.drag = 10;
-	entity.body.force.y = 400;
+	//entity.body.force.y = 400;
 	entity.collider.box = { {0, 0},{30, 30} };
 	entity.sprite.texId = Textures::moonDust;
 	entity.sprite.dim = { 30, 30 };
+
+	bool firstStep = true;
 
 
 	while (sfw::stepContext())
 	{
 		float dt = sfw::getDeltaTime();
+		if (firstStep)
+		{
+			dt = 0;
+			firstStep = false;
+		}
 
 		//entity.body.force.y = -50;
-		entity.body.force.y -= 500;
+		entity.body.force.y -= 1000;
 
 		entity.body.force.x += sfw::getKey('A') * -1000;
 		entity.body.force.x += sfw::getKey('D') * 1000;
-		entity.body.impulse.y += sfw::getKey('W') * 1000 * entity.onGround;
+		entity.body.impulse.y += sfw::getKey('W') * 900 * entity.onGround;
 
 		entity.update(dt);
 		world.collide(entity);
-
+		
 		entity.draw();
 		world.draw();
 		sfw::drawTexture(Textures::background, 400, 300, 800, 600);
